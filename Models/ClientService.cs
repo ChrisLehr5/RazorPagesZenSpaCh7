5dd831f2935d099f5e45cafe3aac0c8b47028060
chrislargent <christine.com>
2020-10-20 18:29:13 -0400

    Updated

---
+++ Models/ClientService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPages.Models
+{
+    public class ClientService
+    {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey("ClientID")]
+        public int ClientID { get; set; }
+        [ForeignKey("ServicesID")]
+        public int ServicesID { get; set; }
+        public Services Services { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Contact email")]
+        public DateTime Date { get; set; }
+    }
+}
